6818d9dc01b0dc771de00b5225baae899f945bdf
Plotnikov Anton <plotnikovanton@gmail.com>
2016-09-21 11:19:12 +0300

    Reorganize repository structure & systemd service
    - Oracle response files moved to `ora-response` subdirrectory
    - Shell scripts moved to `scripts` directory
    - Environment settings for oracle user moved from `.bash_profile` to `.bashrc`
    - Added proxy configuration template to `Vagrantfile`
    - `startup.sh` replaced with `oracle-rdmbs.service` which uses
      `dbstart` and `dbhalt` oracle's utilities

--- startup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-echo 'STARTUP: Starting Oracle'
-echo "startup;" > /vagrant/startup.sql
-su -l oracle -c "sqlplus / as sysdba @/vagrant/startup.sql"
-su -l oracle -c "lsnrctl start"
-rm /vagrant/startup.sql
